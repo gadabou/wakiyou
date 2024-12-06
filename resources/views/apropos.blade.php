ab8b8f36654fd9489109f15d7fa2f9de44a023e7
Hafissou <hafissouagbere904@gmail.com>
2024-12-06 23:19:57 +0000

    Modification des images

--- resources/views/apropos.blade.php
+++ resources/views/apropos.blade.php
@@ -156,15 +156,15 @@
     </div>
     <!-- About End -->
 
-    <!-- Team Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+            <!-- Team Start -->
+            <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
                 <h5 class="fw-bold text-uppercase">Membres de l'équipe</h5>
                 <h1 class="mb-0">Voici les vaillants membres de l'équipe</h1>
             </div>
             <div class="row g-5">
-                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
+                <div class="col-lg-3 wow slideInUp" data-wow-delay="0.3s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
                             <img class="img-fluid w-100" src="{{asset('img/docteur2.jpg')}}" alt="">
@@ -181,7 +181,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
+                <div class="col-lg-3 wow slideInUp" data-wow-delay="0.6s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
                             <img class="img-fluid w-100" src="{{asset('img/team-2.jpg')}}" alt="">
@@ -193,12 +193,12 @@
                             </div>
                         </div>
                         <div class="text-center py-4">
-                            <h4 class=""style="color: #E05206">Col. X</h4>
-                            <p class="text-uppercase m-0">Docteur en médécine et ingénieur en informatique médicale</p>
+                            <h4 class="" style="color: #E05206">SIMPORE Abdoul Aziz</h4>
+                            <p class="text-uppercase m-0">Ingénieur en informatique</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <div class="col-lg-3 wow slideInUp" data-wow-delay="0.9s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
                             <img class="img-fluid w-100" src="{{asset('img/gado2.jpg')}}" alt="">
@@ -210,8 +210,25 @@
                             </div>
                         </div>
                         <div class="text-center py-4">
-                            <h4 class=""style="color: #E05206">GADO Djakpo</h4>
+                            <h4 class="" style="color: #E05206">GADO Djakpo</h4>
                             <p class="text-uppercase m-0">Ingénieur génie logiciel</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 wow slideInUp" data-wow-delay="1.2s">
+                    <div class="team-item bg-light rounded overflow-hidden">
+                        <div class="team-img position-relative overflow-hidden">
+                            <img class="img-fluid w-100" src="{{asset('img/team-3.jpg')}}" alt="">
+                            <div class="team-social">
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                            </div>
+                        </div>
+                        <div class="text-center py-4">
+                            <h4 class="" style="color: #E05206">SAWADOGO Abdoul Salam</h4>
+                            <p class="text-uppercase m-0">Ingénieur informatique</p>
                         </div>
                     </div>
                 </div>
@@ -219,7 +236,7 @@
         </div>
     </div>
     <!-- Team End -->
-
+     
     <!-- Vendor Start -->
     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5 mb-5">
